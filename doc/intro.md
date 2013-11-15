30f2db26538699cd09154659fa7452a55c2144cf
Peter Christensen <peter@pchristensen.com>
2013-11-14 22:12:22 -0800

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to general_conference_podcast
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/general_conference_podcast/core_test.clj
@@ -0,0 +1,7 @@
+(ns general-conference-podcast.core-test
+  (:require [clojure.test :refer :all]
+            [general-conference-podcast.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
